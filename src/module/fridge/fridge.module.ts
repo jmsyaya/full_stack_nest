af4beacffc0be6fc7a7a86aa834c93c480edb9f1
dev-lyj9703070 <lyj9703070@gmail.com>
2026-03-30 16:33:21 +0900

    feat: 냉장고 재료 기반 레시피상세페이지 API 구현 및 프론트 연동

--- src/module/fridge/fridge.module.ts
+++ src/module/fridge/fridge.module.ts
@@ -3,10 +3,11 @@ import { PrismaService } from 'src/service/prisma/prisma.service';
 import { OpenaiModule } from '../openai/openai.module';
 import { FridgeController } from 'src/controller/fridge/fridge.controller';
 import { FridgeService } from 'src/service/fridge/fridge.service';
+import { ImageService } from 'src/service/image/image.service';
 
 @Module({
   imports: [OpenaiModule], 
   controllers: [FridgeController],
-  providers: [FridgeService, PrismaService],
+  providers: [FridgeService, PrismaService, ImageService],
 })
 export class FridgeModule {}
